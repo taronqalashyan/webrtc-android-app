986861534df45a76d97b4009a61d268ee500b693
Smbat Sargsyan <smbatsargsyan1992@gmail.com>
2018-09-01 00:16:48 +0400

    Added initial version of sample app

---
+++ WebRTCAndroidApp/app/src/main/java/com/ss/solutions/call/OnCallEvents.java
@@ -0,0 +1,14 @@
+package com.ss.solutions.call;
+
+/**
+ * Call control interface for container activity.
+ */
+public interface OnCallEvents {
+    void onCallHangUp();
+
+    void onCameraSwitch();
+
+    void onCaptureFormatChange(int width, int height, int framerate);
+
+    boolean onToggleMic();
+}
